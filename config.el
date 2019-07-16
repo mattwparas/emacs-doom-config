fdffb4091b3287c380ffb8d7212a7e64ca287bb6
Matthew Paras <mparas@wayfair.com>
2019-07-16 08:45:03 -0400

    work doom config

---
+++ config.el
@@ -0,0 +1,225 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(after! neotree
+  (setq doom-themes-neotree-file-icons 'icons)
+  (setq doom-themes-neotree-enable-file-icons 'icons)
+  (setq neo-theme 'icons))
+
+(after! doom-themes
+  (setq doom-neotree-file-icons t))
+
+(after! ivy
+  (setq ivy-re-builders-alist
+        '((t . ivy--regex-ignore-order))))
+
+(after! projectile
+  (setq projectile-create-missing-test-files t)
+  (projectile-register-project-type 'haskell-stack '("stack.yaml")
+                                    :compile "stack build"
+                                    :test "stack build --test"
+                                    :test-suffix "Test")
+  (projectile-mode)
+  (projectile-load-known-projects))
+
+
+(def-package! rust-mode
+  :mode "\\.rs$"
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+  (setq rust-format-on-save t)
+  (flycheck-mode))
+
+;; (add-hook! flycheck-rust
+;;   :after rust-mode
+;;   :config
+;;   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+;; (def-package! racer
+;;   :after rust-mode
+;;   :config
+;;   (setq racer-rust-src-path
+;;         (concat
+;;          (replace-regexp-in-string "\n$" "" (shell-command-to-string "rustc --print sysroot"))
+;;          "/lib/rustlib/src/rust/src"))
+;;   (company-mode)
+;;   (eldoc-mode))
+
+(def-package! flycheck-mix
+  :after elixir-mode
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-mix-setup))
+
+(def-package! flycheck-credo
+  :after elixir-mode
+  :config
+  (setq flycheck-elixir-credo-strict t)
+  (add-hook 'flycheck-mode-hook #'flycheck-credo-setup))
+
+(def-package! lux-mode
+  :mode "\\.lux$")
+
+(def-package! erlang
+  :mode "\\.erl$"
+  :config
+  (erlang-mode))
+
+(def-package! racket-mode
+  :mode "\\.rkt$"
+  :config
+  (company-mode)
+  (flycheck-mode)
+  (rainbow-delimiters-mode))
+
+(def-package! aggressive-indent
+  :hook
+  (clojure-mode . aggressive-indent-mode)
+  (hy-mode . aggressive-indent-mode)
+  (lisp-mode . aggressive-indent-mode))
+
+(after! clojure-mode
+  (define-clojure-indent
+    (PUT 2)
+    (POST 2)
+    (GET 2)
+    (PATCH 2)
+    (DELETE 2)
+    (context 2)
+    (for-all 2)
+    (checking 3))
+  (setq clojure-align-forms-automatically t)
+  (setq cider-cljs-lein-repl
+        "(do (require 'figwheel-sidecar.repl-api)
+         (figwheel-sidecar.repl-api/start-figwheel!)
+         (figwheel-sidecar.repl-api/cljs-repl))")
+  (setq cljr-magic-require-namespaces
+        '(("io" . "clojure.java.io")
+          ("sh" . "clojure.java.shell")
+          ("jdbc" . "clojure.java.jdbc")
+          ("set" . "clojure.set")
+          ("time" . "java-time")
+          ("str" . "cuerdas.core")
+          ("path" . "pathetic.core")
+          ("walk" . "clojure.walk")
+          ("zip" . "clojure.zip")
+          ("async" . "clojure.core.async")
+          ("component" . "com.stuartsierra.component")
+          ("http" . "clj-http.client")
+          ("url" . "cemerick.url" )
+          ("sql" . "honeysql.core")
+          ("csv" . "clojure.data.csv")
+          ("json" . "cheshire.core")
+          ("s" . "clojure.spec.alpha")
+          ("fs" . "me.raynes.fs")
+          ("ig" . "integrant.core")
+          ("cp" . "com.climate.claypoole")
+          ("re-frame" . "re-frame.core")
+          ("rf"       . "re-frame.core")
+          ("re"       . "reagent.core")
+          ("reagent"  . "reagent.core")
+          ("u.core"   . "utopia.core"))))
+
+(def-package! graphql-mode
+  :mode "\\.gql$")
+
+(def-package! lsp-mode
+  :hook
+  (haskell-mode . lsp)
+  (python-mode . lsp)
+  (rust-mode . lsp)
+  :commands
+  lsp)
+
+(def-package! lsp-ui
+  :commands
+  lsp-ui-mode)
+
+(def-package! company-lsp
+  :commands company-lsp)
+
+(def-package! lsp-haskell
+  :after haskell-mode
+  :config
+  (setq lsp-haskell-process-path-hie "hie-wrapper"))
+
+(def-package! yapfify
+  :hook
+  (python-mode . yapf-mode)
+  (before-save . yapify-buffer))
+
+
+(def-package! haskell-mode
+  :mode "\\.hs$"
+  :config
+  (rainbow-delimiters-mode)
+  ;; (setq haskell-font-lock-symbols t)
+  (add-to-list ("<>" . "⊕"))
+  (setq haskell-font-lock-symbols-alist
+        (-reject
+         (lambda (elem)
+           (or))
+         ;; (string-equal "::" (car elem))))
+         haskell-font-lock-symbols-alist)))
+
+(def-package! agda2-mode
+  :mode "\\.agda$"
+  :interpreter "agda2"
+  :preface
+  (let ((agda-mode-directory
+         (file-name-directory
+          (shell-command-to-string "agda-mode locate"))))
+    (add-to-list 'load-path agda-mode-directory)
+    (let ((inhibit-message t))
+      (byte-recompile-directory agda-mode-directory 0)))
+  :config
+  (set-face-attribute 'agda2-highlight-bound-variable-face nil
+                      :foreground "#ede0ce"
+                      :slant 'italic)
+  (set-face-attribute 'agda2-highlight-catchall-clause-face nil
+                      :overline "#7bc275")
+  (set-face-attribute 'agda2-highlight-coinductive-constructor-face nil
+                      :foreground "#c57bdb")
+  (set-face-attribute 'agda2-highlight-coverage-problem-face nil
+                      :box '(:line-width 1 :color "#5cefff"))
+  (set-face-attribute 'agda2-highlight-datatype-face nil
+                      :foreground "#fcce7b")
+  (set-face-attribute 'agda2-highlight-dotted-face nil
+                      :foreground "#5e5e5e")
+  (set-face-attribute 'agda2-highlight-error-face nil
+                      :strike-through "#c82829")
+  (set-face-attribute 'agda2-highlight-field-face nil
+                      :foreground "#7bc275")
+  (set-face-attribute 'agda2-highlight-function-face nil
+                      :foreground "#ff5555")
+  (set-face-attribute 'agda2-highlight-inductive-constructor-face nil
+                      :foreground "#fb2874")
+  (set-face-attribute 'agda2-highlight-keyword-face nil
+                      :foreground "#268bd2"
+                      :underline nil)
+  (set-face-attribute 'agda2-highlight-macro-face nil
+                      :foreground "#99bb66")
+  (set-face-attribute 'agda2-highlight-module-face nil
+                      :foreground "#d33682")
+  (set-face-attribute 'agda2-highlight-number-face nil
+                      :foreground "#7bc275")
+  (set-face-attribute 'agda2-highlight-positivity-problem-face nil
+                      :box '(:line-width 2 :color "#f6bfbc"))
+  (set-face-attribute 'agda2-highlight-postulate-face nil
+                      :foreground "#51afef")
+  (set-face-attribute 'agda2-highlight-primitive-face nil
+                      :foreground "#c57bdb")
+  (set-face-attribute 'agda2-highlight-primitive-type-face nil
+                      :foreground "#c57bdb")
+  (set-face-attribute 'agda2-highlight-record-face nil
+                      :foreground "#fcce7B")
+  (set-face-attribute 'agda2-highlight-string-face nil
+                      :foreground "#7bc275")
+  (set-face-attribute 'agda2-highlight-symbol-face nil
+                      :foreground "#5B6268")
+  (set-face-attribute 'agda2-highlight-termination-problem-face nil
+                      :box '(:line-width 2 :color "#a60033"))
+  (set-face-attribute 'agda2-highlight-unsolved-constraint-face nil
+                      :strike-through "#c57bdb")
+  (set-face-attribute 'agda2-highlight-unsolved-meta-face nil
+                      :box '(:line-width 1 :color "#fcce7B")))
---
+++ init.el
@@ -0,0 +1,225 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+;; Copy this file to ~/.doom.d/init.el or ~/.config/doom/init.el ('doom
+;; quickstart' will do this for you). The `doom!' block below controls what
+;; modules are enabled and in what order they will be loaded. Remember to run
+;; 'doom refresh' after modifying it.
+;;
+;; More information about these modules (and what flags they support) can be
+;; found in modules/README.org.
+
+(doom! :input
+       ;;chinese
+       ;;japanese
+
+       :completion
+       company           ; the ultimate code completion backend
+       ;;helm              ; the *other* search engine for love and life
+       ;;ido               ; the other *other* search engine...
+       ivy               ; a search engine for love and life
+
+       :ui
+       ;;deft              ; notational velocity for Emacs
+       doom              ; what makes DOOM look the way it does
+       doom-dashboard    ; a nifty splash screen for Emacs
+       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;fill-column       ; a `fill-column' indicator
+       hl-todo           ; highlight TODO/FIXME/NOTE tags
+       ;;indent-guides     ; highlighted indent columns
+       modeline          ; snazzy, Atom-inspired modeline, plus API
+       nav-flash         ; blink the current line after jumping
+       neotree           ; a project drawer, like NERDTree for vim
+       ophints           ; highlight the region an operation acts on
+       (popup            ; tame sudden yet inevitable temporary windows
+        +all             ; catch all popups that start with an asterix
+        +defaults)       ; default popup rules
+       ;;pretty-code       ; replace bits of code with pretty symbols
+       ;;tabbar            ; FIXME an (incomplete) tab bar for Emacs
+       treemacs          ; a project drawer, like neotree but cooler
+       unicode           ; extended unicode support for various languages
+       vc-gutter         ; vcs diff in the fringe
+       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       window-select     ; visually switch windows
+       workspaces        ; tab emulation, persistence & separate workspaces
+
+       :editor
+       (evil +everywhere); come to the dark side, we have cookies
+       file-templates    ; auto-snippets for empty files
+       fold              ; (nigh) universal code folding
+       ;;(format +onsave)  ; automated prettiness
+       ;;lispy             ; vim for lisp, for people who dont like vim
+       multiple-cursors  ; editing in many places at once
+       ;;objed             ; text object editing for the innocent
+       ;;parinfer          ; turn lisp into python, sort of
+       rotate-text       ; cycle region at point between text candidates
+       snippets          ; my elves. They type so I don't have to
+
+       :emacs
+       (dired            ; making dired pretty [functional]
+       ;;+ranger         ; bringing the goodness of ranger to dired
+       ;;+icons          ; colorful icons for dired-mode
+        )
+       electric          ; smarter, keyword-based electric-indent
+       vc                ; version-control and Emacs, sitting in a tree
+
+       :term
+       ;;eshell            ; a consistent, cross-platform shell (WIP)
+       ;;term              ; terminals in Emacs
+       ;;vterm             ; another terminals in Emacs
+
+       :tools
+       ;;ansible
+       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       ;;direnv
+       ;;docker
+       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       ;;ein               ; tame Jupyter notebooks with emacs
+       eval              ; run code, run (also, repls)
+       flycheck          ; tasing you for every semicolon you forget
+       ;;flyspell          ; tasing you for misspelling mispelling
+       ;;gist              ; interacting with github gists
+       (lookup           ; helps you navigate your code and documentation
+        +docsets)        ; ...or in Dash docsets locally
+       ;;lsp
+       ;;macos             ; MacOS-specific commands
+       magit             ; a git porcelain for Emacs
+       ;;make              ; run make tasks from Emacs
+       ;;pass              ; password manager for nerds
+       ;;pdf               ; pdf enhancements
+       ;;prodigy           ; FIXME managing external services & code builders
+       ;;rgb               ; creating color strings
+       ;;terraform         ; infrastructure as code
+       ;;tmux              ; an API for interacting with tmux
+       ;;upload            ; map local to remote projects via ssh/ftp
+       ;;wakatime
+
+       :lang
+       agda              ; types of types of types of types...
+       ;;assembly          ; assembly for fun or debugging
+       ;;cc                ; C/C++/Obj-C madness
+       clojure           ; java with a lisp
+       common-lisp       ; if you've seen one lisp, you've seen them all
+       ;;coq               ; proofs-as-programs
+       ;;crystal           ; ruby at the speed of c
+       ;;csharp            ; unity, .NET, and mono shenanigans
+       data              ; config/data formats
+       ;;erlang            ; an elegant language for a more civilized age
+       ;;elixir            ; erlang done right
+       ;;elm               ; care for a cup of TEA?
+       emacs-lisp        ; drown in parentheses
+       ;;ess               ; emacs speaks statistics
+       ;;fsharp           ; ML stands for Microsoft's Language
+       go                ; the hipster dialect
+       (haskell +intero) ; a language that's lazier than I am
+       ;;hy                ; readability of scheme w/ speed of python
+       ;;idris             ;
+       ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
+       javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       ;;julia             ; a better, faster MATLAB
+       kotlin            ; a better, slicker Java(Script)
+       ;;latex             ; writing papers in Emacs has never been so fun
+       ;;ledger            ; an accounting system in Emacs
+       ;;lua               ; one-based indices? one-based indices
+       markdown          ; writing docs for people to ignore
+       ;;nim               ; python + lisp at the speed of c
+       ;;nix               ; I hereby declare "nix geht mehr!"
+       ;;ocaml             ; an objective camel
+       (org              ; organize your plain life in plain text
+        +dragndrop       ; file drag & drop support
+        +ipython         ; ipython support for babel
+        +pandoc          ; pandoc integration into org's exporter
+        +present)        ; using Emacs for presentations
+       ;;perl              ; write code no one else can comprehend
+       ;;php               ; perl's insecure younger brother
+       ;;plantuml          ; diagrams for confusing people more
+       ;;purescript        ; javascript, but functional
+       python            ; beautiful is better than ugly
+       ;;qt                ; the 'cutest' gui framework ever
+       racket            ; a DSL for DSLs
+       ;;rest              ; Emacs as a REST client
+       ;;ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       scala             ; java, but good
+       sh                ; she sells {ba,z,fi}sh shells on the C xor
+       ;;solidity          ; do you need a blockchain? No.
+       swift             ; who asked for emoji variables?
+       ;;terra             ; Earth and Moon in alignment for performance.
+       ;;web               ; the tubes
+       ;;vala              ; GObjective-C
+
+       :email
+       ;;(mu4e +gmail)       ; WIP
+       ;;notmuch             ; WIP
+       ;;(wanderlust +gmail) ; WIP
+
+       ;; Applications are complex and opinionated modules that transform Emacs
+       ;; toward a specific purpose. They may have additional dependencies and
+       ;; should be loaded late.
+       :app
+       ;;calendar
+       ;;irc              ; how neckbeards socialize
+       ;;(rss +org)        ; emacs as an RSS reader
+       ;;twitter           ; twitter client https://twitter.com/vnought
+       ;;(write            ; emacs as a word processor (latex + org + markdown)
+       ;; +wordnut         ; wordnet (wn) search
+       ;; +langtool)       ; a proofreader (grammar/style check) for Emacs
+
+       :collab
+       ;;floobits          ; peer programming for a price
+       ;;impatient-mode    ; show off code over HTTP
+
+       :config
+       ;; For literate config users. This will tangle+compile a config.org
+       ;; literate config in your `doom-private-dir' whenever it changes.
+       ;;literate
+
+       ;; The default module sets reasonable defaults for Emacs. It also
+       ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
+       ;; config. Use it as a reference for your own modules.
+       (default +bindings +smartparens))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#21242b" "#BF616A" "#A3BE8C" "#ECBE7B" "#8FA1B3" "#c678dd" "#46D9FF" "#c0c5ce"])
+ '(custom-enabled-themes (quote (doom-Iosvkem)))
+ '(custom-safe-themes
+   (quote
+    ("43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "9954ed41d89d2dcf601c8e7499b6bb2778180bfcaeb7cdfc648078b8e05348c6" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "84da7b37214b4ac095a55518502dfa82633bee74f64daf6e1785322e77516f96" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "256bd513a9875cd855077162cdfee8d75b0ad7e18fe8b8cbc10412561fbef892" "fc924589b9eb9f7d02133f0c32d6a19a92638ba405f699f0f44abc6c0941dccb" "80365dd15f97396bdc38490390c23337063c8965c4556b8f50937e63b5e9a65c" "6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092" default)))
+ '(fci-rule-color "#65737E")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#D08770"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#A3BE8C"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#4f5b66"))
+ '(objed-cursor-color "#BF616A")
+ '(vc-annotate-background "#2F3841")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A3BE8C")
+    (cons 40 "#bbbe86")
+    (cons 60 "#d3be80")
+    (cons 80 "#ECBE7B")
+    (cons 100 "#e2ab77")
+    (cons 120 "#d99973")
+    (cons 140 "#D08770")
+    (cons 160 "#cc8294")
+    (cons 180 "#c97db8")
+    (cons 200 "#c678dd")
+    (cons 220 "#c370b6")
+    (cons 240 "#c16890")
+    (cons 260 "#BF616A")
+    (cons 280 "#a35f69")
+    (cons 300 "#875e68")
+    (cons 320 "#6b5c67")
+    (cons 340 "#65737E")
+    (cons 360 "#65737E")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
